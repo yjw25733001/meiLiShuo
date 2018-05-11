b1bfb60554e6d663a4a74dc57a602de0db22061b
yjw25733001 <34611866+yjw25733001@users.noreply.github.com>
2018-05-12 02:10:14 +0800

    Revert "123"
    This reverts commit 8e4cf36e8199de7409df3f8d6de66536cee8d186.

--- javascript/choose.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-$(function(){
- $(".tab_menu ul li").click(function(){
- $(this).addClass("on").siblings().removeClass("on"); 
- var index=$(this).index(); 
- $(".search_box > div").eq(index).show().siblings().hide(); 
- });
-});
--- javascript/my.js
+++
@@ -1,7 +0,0 @@
-var sum;
-$("[name^=j]").mousemove(function (){
-    sum = $("[name^=j]").index(this);
-    $(this).addClass("sel").siblings().removeClass("sel");
-    $("[name^=i]").hide();
-    $("[name*=i]").eq(sum).show();
-});
